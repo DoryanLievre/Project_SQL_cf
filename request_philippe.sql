b7bad676ce0acd1fbef23604276e9a8ca453d411
Doryan Lièvre <doryan.lievre@edu.itescia.fr>
2018-10-23 11:34:13 +0200

    real update

--- request_philippe.sql
+++
@@ -1,9 +0,0 @@
--- Inscription ( users ) -- 
-
-INSERT INTO users (mail, pass, nickname, date_inscription)  -- PERMET D AJOUTER UNE LIGNE 
-	VALUES ("Philippe@coding.fr", "Pass", "Phillou", "2018-09-23 12:00:00");
-
--- Connexion ( users )
-
-SELECT * FROM users WHERE mail = "Philippe@coding.fr" AND pass = "pass"; -- REQUETE :cherche dans la table users dans la colonne "mail" et "pass"
-																		-- si la condition est respecté cela entraine une connexion  
